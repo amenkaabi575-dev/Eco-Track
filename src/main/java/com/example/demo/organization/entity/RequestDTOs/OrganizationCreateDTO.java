97464d0559d015bf918fe5d2e9c9892751bd2aac
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-06-07 19:08:39 +0100

    finished Organization DTOs classes validation

--- src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationCreateDTO.java
+++ src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationCreateDTO.java
@@ -3,6 +3,7 @@ package com.example.demo.organization.entity.RequestDTOs;
 import com.example.demo.common.validation.TunisianTaxId;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +15,16 @@ import lombok.NoArgsConstructor;
 @Builder
 public class OrganizationCreateDTO {
 
-    @NotBlank
+    @NotBlank(message = "Organization name is required")
+    @Size(max = 255, message = "Organization name must not exceed 255 characters")
     private String name;
 
-    @NotNull
+    @NotBlank(message = "Tax ID is required")
     @TunisianTaxId
     private String taxId;
 
-    @NotBlank
+    @NotBlank(message = "Sector is required")
+    @Size(max = 255, message = "Sector must not exceed 255 characters")
     private String sector;
 
 }
--- src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationUpdateDTO.java
+++ src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationUpdateDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.organization.entity.RequestDTOs;
 
 import com.example.demo.common.validation.TunisianTaxId;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class OrganizationUpdateDTO {
 
+    @Size(max = 255, message = "Organization name must not exceed 255 characters")
     private String name;
 
-    @TunisianTaxId
-    private String taxId;
-
+    @Size(max = 255, message = "Sector must not exceed 255 characters")
     private String Sector;
 
 }
