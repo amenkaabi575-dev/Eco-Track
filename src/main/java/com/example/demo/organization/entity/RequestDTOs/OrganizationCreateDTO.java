0f2fe5856ae3c3de04a837777373f4776f3edb45
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-06-07 18:49:24 +0100

    add builder to DTOs

--- src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationCreateDTO.java
+++ src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationCreateDTO.java
@@ -4,12 +4,14 @@ import com.example.demo.common.validation.TunisianTaxId;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrganizationCreateDTO {
 
     @NotBlank
--- src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationUpdateDTO.java
+++ src/main/java/com/example/demo/organization/entity/RequestDTOs/OrganizationUpdateDTO.java
@@ -2,12 +2,14 @@ package com.example.demo.organization.entity.RequestDTOs;
 
 import com.example.demo.common.validation.TunisianTaxId;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrganizationUpdateDTO {
 
     private String name;
