1521addfbcf249b3e2027028bc8705a70c39aa68
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-06-08 23:32:41 +0100

    handle validation and type mismatch exceptions

--- src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,20 @@
 package com.example.demo.common.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,7 +22,7 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex, HttpServletRequest request){
 
         ErrorResponse response = ErrorResponse.builder()
                 .message(ex.getMessage())
@@ -29,7 +37,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleBusiness(BusinessException ex, HttpServletRequest request){
 
 
         ErrorResponse response = ErrorResponse.builder()
@@ -42,6 +50,69 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(ex.getStatus()).body(response);
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request){
+
+        Map<String,String> errors = ex.getBindingResult()
+                .getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                    FieldError::getField,
+                    FieldError::getDefaultMessage
+        ));
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message("Validation Failed")
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .errorCode("VALIDATION_FAILED")
+                .path(request.getRequestURI())
+                .errors(errors)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException ex, HttpServletRequest request){
+
+        Map<String,String> errors = ex
+                .getConstraintViolations()
+                .stream()
+                .collect(Collectors.toMap(
+                        constraintViolation -> constraintViolation.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+
+                ));
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message("Validation Failed")
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .errorCode("CONSTRAINT_VIOLATED")
+                .path(request.getRequestURI())
+                .errors(errors)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, HttpServletRequest request){
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message("Property type is not correct")
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .errorCode("INCORRECT_PROPERTY_TYPE")
+                .path(request.getRequestURI())
+                .build();
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
 }
