258a9268ea7257d15677493129bfc8de3bc9a66f
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-06-11 23:10:35 +0100

    add initial user model

---
+++ src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username",unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    
+
+
+
+}
---
+++ src/main/java/com/example/demo/user/UserRole.java
@@ -0,0 +1,10 @@
+package com.example.demo.user;
+
+public enum UserRole {
+
+    ADMIN,
+    FIRM_OWNER,
+    MANAGER,
+    USER
+
+}
