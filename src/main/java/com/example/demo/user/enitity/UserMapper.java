b80a19af3848da4959757a29bfd9039bea1c4ee3
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-06-12 15:52:53 +0100

    add User Mapper

--- src/main/java/com/example/demo/user/enitity/UserMapper.java
+++ src/main/java/com/example/demo/user/enitity/UserMapper.java
@@ -1,11 +1,24 @@
 package com.example.demo.user.enitity;
 
 
+import com.example.demo.user.enitity.requestDTOs.UserCreateDTO;
+import com.example.demo.user.enitity.requestDTOs.UserUpdateDTO;
+import com.example.demo.user.enitity.responseDTOs.UserDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
+    @Mapping(source = "organization.id", target = "organizationId")
+    UserDTO toDto(User user);
 
+    @Mapping(target = "organization", ignore = true)
+    User toEntity(UserCreateDTO dto);
+
+    @Mapping(target = "organization", ignore = true)
+    void updateEntityFromDto(UserUpdateDTO dto, @MappingTarget User user);
 
 }
