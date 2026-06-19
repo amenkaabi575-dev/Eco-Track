d1ecf7914ab9a990d9e3cab0584368e272ae41a6
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-06-19 23:56:25 +0100

    Add Analytics DTOs

---
+++ src/main/java/com/example/demo/activity/entity/responseDTOs/AssetEmissionDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.activity.entity.responseDTOs;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AssetEmissionDTO {
+
+
+    private UUID assetId;
+    private String assetName;
+    private BigDecimal totalCO2;
+
+}
