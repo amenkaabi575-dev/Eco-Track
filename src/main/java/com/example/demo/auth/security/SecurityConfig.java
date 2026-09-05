10f652fca272ff288682677a7c88f7de25305d48
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-09-05 21:37:11 +0100

    Add RSA key pair

--- src/main/java/com/example/demo/auth/security/SecurityConfig.java
+++ src/main/java/com/example/demo/auth/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.auth.security;
 
 
-import jakarta.servlet.http.HttpServlet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
